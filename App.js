f46788a33e97426339e2dc25a9b52214edef0d1d
Alec <alecperkey@gmail.com>
2017-09-19 18:44:50 -0700

    add and initilize firebase app

--- App.js
+++ App.js
@@ -2,8 +2,22 @@
 
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import firebase from 'firebase';
 
 export default class App extends React.Component {
+  componentDidMount() {
+    // Initialize Firebase
+    var config = {
+      apiKey: "AIzaSyDIP0UGGNRqFP946TWk3CcfWtwKO7Yyn_s",
+      authDomain: "positive-head.firebaseapp.com",
+      databaseURL: "https://positive-head.firebaseio.com",
+      projectId: "positive-head",
+      storageBucket: "positive-head.appspot.com",
+      messagingSenderId: "770866405055"
+    };
+    firebase.initializeApp(config);
+  }
+
   render() {
     return (
       <View style={styles.container}>
